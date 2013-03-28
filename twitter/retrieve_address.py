2fb3b94a8f457d459db80a07634e3a63819a7d5e
David Jones <jonesd52@arctic.cse.msu.edu>
2013-03-28 15:36:24 -0400

    merged with Titus' code

--- twitter/retrieve_address.py
+++ twitter/retrieve_address.py
@@ -5,6 +5,7 @@ Use the Twitter API to reverse geocode
 import urllib, json, pprint
 
 params = urllib.urlencode(dict(lat=37.76893497, long=-122.42284884))
+#params = urllib.urlencode(dict(lat=38.8921037, long=-77.02596130))
 
 u = urllib.urlopen('https://api.twitter.com/1/geo/reverse_geocode.json?' + \
                    params)
